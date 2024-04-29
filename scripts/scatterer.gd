cdfc1020a5a6a9b5f949d765d5cefdb045eb5e6e
B-BARTHMAN <bribri.barth@gmail.com>
2024-04-29 10:57:08 +0200

    scattered some rocks

--- scripts/scatterer.gd
+++ scripts/scatterer.gd
@@ -1,38 +1,31 @@
-#@tool
-extends Node3D
+@tool
+extends MultiMeshInstance3D
 
 @export var height : float = 2.5
 @export var height_map : NoiseTexture2D
-
-@export var rock : PackedScene
-@export var rock_count : int = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	if Engine.is_editor_hint():
 		
-		print("removing all objects")
-		
-		for n in get_children():
-			remove_child(n)
-		
-		print("placing objects")
 		await height_map.changed
 		
 		var img : Image = height_map.get_image()
 		
-		for i in range(rock_count):
+		var arr : PackedFloat32Array = multimesh.buffer.duplicate()
+		
+		for i in range(multimesh.instance_count):
 			
-			var randx = randf_range(-50, 50)
-			var randz = randf_range(-50, 50)
+			var randx = arr[12 * i + 3]
+			var randz = arr[12 * i + 11]
 			
 			var randxi : int = clampi(roundi((randx + 50.)/100.*512.), 0, 511)
 			var randzi : int = clampi(roundi((randz + 50.)/100.*512.), 0, 511)
 			
 			var h : float = img.get_pixel(randxi, randzi).r * height
 			
-			var r := rock.instantiate()
-			add_child(r)
-			r.global_position = Vector3(randx, h, randz)
+			arr.set(12 * i + 7, h)
+			multimesh.buffer = arr
+			#r.global_position = Vector3(randx, h, randz)
 
