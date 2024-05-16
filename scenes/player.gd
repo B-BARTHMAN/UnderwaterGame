d085c8c3dbefde3c35b1a8070a4bb54db10f7954
B-BARTHMAN <bribri.barth@gmail.com>
2024-05-16 13:57:30 +0200

    object pickup and movement working

--- scenes/player.gd
+++ scenes/player.gd
@@ -6,6 +6,13 @@ extends XROrigin3D
 @export var righthand : XRController3D
 @export var camera : XRCamera3D
 
+var lh_full : bool = false
+var rh_full : bool = false
+@export var lh_slot : Node3D
+@export var rh_slot : Node3D
+
+@export var all_trash : Node3D
+
 @export var left_collision : Area3D
 var left_objects = []
 @export var right_collision : Area3D
@@ -27,12 +34,48 @@ func _process(delta):
 	if left_stick.y > 0.25:
 		global_position -= delta * left_stick.y * swim_speed * camera.global_transform.basis.z
 	
-	if left_grab > 0.5:
+	if left_grab > 0.9 and not lh_full:
 		for v in left_objects:
-			v.queue_free()
-	if right_grab > 0.5:
+			var p = v.global_position
+			var r = v.global_rotation
+			all_trash.remove_child(v)
+			lh_slot.add_child(v)
+			v.collision_layer = 4
+			v.global_position = p
+			v.global_rotation = r
+			lh_full = true
+			break
+	if left_grab < 0.1 and lh_full:
+		var obj = lh_slot.get_child(0)
+		var p = obj.global_position
+		var r = obj.global_rotation
+		lh_slot.remove_child(obj)
+		all_trash.add_child(obj)
+		obj.collision_layer = 2
+		obj.global_position = p
+		obj.global_rotation = r
+		lh_full = false
+	if right_grab > 0.9 and not rh_full:
 		for v in right_objects:
-			v.queue_free()
+			var p = v.global_position
+			var r = v.global_rotation
+			all_trash.remove_child(v)
+			rh_slot.add_child(v)
+			v.collision_layer = 4
+			v.global_position = p
+			v.global_rotation = r
+			rh_full = true
+			break
+	if right_grab < 0.1 and rh_full:
+		var obj = rh_slot.get_child(0)
+		var p = obj.global_position
+		var r = obj.global_rotation
+		rh_slot.remove_child(obj)
+		all_trash.add_child(obj)
+		obj.collision_layer = 2
+		obj.global_position = p
+		obj.global_rotation = r
+		rh_full = false
 
 func _on_grabarea_r_body_entered(body):
 	right_objects.append(body)
@@ -45,3 +88,14 @@ func _on_grabarea_l_body_entered(body):
 
 func _on_grabarea_l_body_exited(body):
 	left_objects.erase(body)
+
+func _on_trashbag_body_entered(body):
+	
+	for obj in rh_slot.get_children():
+		if obj == body:
+			body.queue_free()
+			rh_full = false
+	for obj in lh_slot.get_children():
+		if obj == body:
+			body.queue_free()
+			lh_full = false
